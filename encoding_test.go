b9850fbf42f343d72b40669337dd190b1579aba9
ejuju <julien_sellier@outlook.com>
2022-03-12 02:11:15 +0100

    added test for encoding

--- encoding_test.go
+++ encoding_test.go
@@ -1,1 +1,35 @@
 package gdb
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	data := "hello"
+	encoded, err := encode(data)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	expected := []byte{34, 104, 101, 108, 108, 111, 34, 10}
+	if !reflect.DeepEqual(encoded, expected) {
+		t.Error("unexpected encoded result")
+		return
+	}
+}
+
+func TestDecode(t *testing.T) {
+	data := []byte{34, 104, 101, 108, 108, 111, 34, 10}
+	str := ""
+	err := decode(data, &str)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	expected := "hello"
+	if str != expected {
+		t.Error("unexpected decoded result")
+		return
+	}
+}
